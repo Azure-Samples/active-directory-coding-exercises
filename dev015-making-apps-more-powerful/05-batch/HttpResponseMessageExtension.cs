e3abe4292f38ae68744d9564c7a415d9251f8af9
Sriram Dhanasekaran <sriramd@microsoft.com>
2018-10-09 00:34:08 +0100

    Batch excercice

---
+++ dev015-making-apps-more-powerful/05-batch/HttpResponseMessageExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Batch
+{
+    public static class HttpResponseMessageExtension
+    {
+        public static void WriteCodeAndReasonToConsole(this HttpResponseMessage response)
+        {
+            var defaultBGColor = Console.BackgroundColor;
+            var defaultFGColor = Console.ForegroundColor;
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Write(response.StatusCode);
+            }
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write(response.StatusCode);
+                Console.WriteLine(" - " + response.ReasonPhrase);
+            }
+            Console.BackgroundColor = defaultBGColor;
+            Console.ForegroundColor = defaultFGColor;
+            Console.WriteLine();
+        }        
+    }
+}
